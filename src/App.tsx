2f12009989e07e8e352a75b1254f13c40b96c75c
Janielson Moura <janielson_moura20@outlook.com>
2023-06-29 09:56:55 -0400

    feat: clear code not used

--- src/App.tsx
+++ src/App.tsx
@@ -2,33 +2,6 @@ import React, { useCallback, useState } from 'react';
 import InputMask from './componentes/Input/index'
 import './style.css'
 
-
-function formatPhoneNumber(phoneNumber: string) {
-  let formattedNumber = phoneNumber.replace(/[^\d]/g, ''); // Remove todos os caracteres não numéricos
-  let formattedOutput = '';
-
-  // Verifica se o número possui mais de 11 caracteres e adiciona uma vírgula se necessário
-  if (formattedNumber.length > 11) {
-    formattedOutput += formattedNumber.slice(0, -11) + ', ';
-    formattedNumber = formattedNumber.slice(-11);
-  }
-
-  // Formata o número restante
-  const countryCode = formattedNumber.slice(0, 2);
-  const areaCode = formattedNumber.slice(2, 4);
-  const firstPart = formattedNumber.slice(4, 9);
-  const secondPart = formattedNumber.slice(9);
-
-  formattedOutput += `(${countryCode}) ${areaCode}-${firstPart}`;
-  if (secondPart) {
-    formattedOutput += `-${secondPart}`;
-  }
-
-  return formattedOutput;
-}
-
-const data2 = ['(92) 99493-5577', '(92) 99493-5577', '(92) 99493-5577', '(92) 99493-5577']
-
 const App: React.FC = () => {
   const [subscribe, setSubscribe] = useState<string[]>([]);
 
